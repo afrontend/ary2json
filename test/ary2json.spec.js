66f4433c755975bb35184140a97a7ec293264890
afrontend <autoscripts@gmail.com>
2021-03-18 19:15:27 +0900

    Use deepStrictEqual instead of deepEqual

--- test/ary2json.spec.js
+++ test/ary2json.spec.js
@@ -4,7 +4,7 @@ const assert = require('assert')
 describe('ary2json', function () {
   it('generate a ary of text and paths to object', function () {
     const obj = textPathsAryToObj([[ '로그인', 'login', 'path1', 'path2', 'path3' ]], { numberOfLanguageColumn: 2 })
-    assert.deepEqual(obj, [
+    assert.deepStrictEqual(obj, [
         {
           path1: {
             path2: {
@@ -23,7 +23,7 @@ describe('ary2json', function () {
   })
   it('generate a ary of text and paths to object (with default option)', function () {
     const obj = textPathsAryToObj([[ '로그인', 'login', 'path1', 'path2', 'path3' ]])
-    assert.deepEqual(obj, [
+    assert.deepStrictEqual(obj, [
         {
           path1: {
             path2: {
@@ -42,7 +42,7 @@ describe('ary2json', function () {
   })
   it('generate a ary of text and paths to object (has one language column)', function () {
     const obj = textPathsAryToObj([[ '로그인', 'path1', 'path2', 'path3' ]], { numberOfLanguageColumn: 1 })
-    assert.deepEqual(obj, [
+    assert.deepStrictEqual(obj, [
         {
           path1: {
             path2: {
