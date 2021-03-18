f1698ca4b04b9fdc723375df035a3f7955bdfd8e
afrontend <autoscripts@gmail.com>
2021-03-18 19:25:10 +0900

    Update a description of spec

--- test/ary2json.spec.js
+++ test/ary2json.spec.js
@@ -2,26 +2,7 @@ const { textPathsAryToObj, toJSONString } = require('../src/ary2json.js')
 const assert = require('assert')
 
 describe('ary2json', function () {
-  it('generate a ary of text and paths to object', function () {
-    const obj = textPathsAryToObj([[ '로그인', 'login', 'path1', 'path2', 'path3' ]], { numberOfLanguageColumn: 2 })
-    assert.deepStrictEqual(obj, [
-        {
-          path1: {
-            path2: {
-              path3: '로그인'
-            }
-          }
-        },
-        {
-          path1: {
-            path2: {
-              path3: 'login'
-            }
-          }
-        }
-      ])
-  })
-  it('generate a ary of text and paths to object (with default option)', function () {
+  it('a array of text and path to Object (with default language number option)', function () {
     const obj = textPathsAryToObj([[ '로그인', 'login', 'path1', 'path2', 'path3' ]])
     assert.deepStrictEqual(obj, [
         {
@@ -40,7 +21,26 @@ describe('ary2json', function () {
         }
       ])
   })
-  it('generate a ary of text and paths to object (has one language column)', function () {
+  it('a array of text and path to Object (with language number option)', function () {
+    const obj = textPathsAryToObj([[ '로그인', 'login', 'path1', 'path2', 'path3' ]], { numberOfLanguageColumn: 2 })
+    assert.deepStrictEqual(obj, [
+        {
+          path1: {
+            path2: {
+              path3: '로그인'
+            }
+          }
+        },
+        {
+          path1: {
+            path2: {
+              path3: 'login'
+            }
+          }
+        }
+      ])
+  })
+  it('a array of text and path to Object (has one language column)', function () {
     const obj = textPathsAryToObj([[ '로그인', 'path1', 'path2', 'path3' ]], { numberOfLanguageColumn: 1 })
     assert.deepStrictEqual(obj, [
         {
