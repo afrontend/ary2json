cf857e224e6974d1baa71bfe2375a246b4cd598f
afrontend <autoscripts@gmail.com>
2021-03-18 22:38:26 +0900

    Update a title of spec

--- test/ary2json.spec.js
+++ test/ary2json.spec.js
@@ -1,56 +1,54 @@
 const { textPathsAryToObj, toJSONString } = require('../src/ary2json.js')
 const assert = require('assert')
-
-describe('ary2json', function () {
+describe('textPathsAryToObj', function () {
   it('a array of text and path to Object (with default language number option)', function () {
     const obj = textPathsAryToObj([[ '로그인', 'login', 'path1', 'path2', 'path3' ]])
     assert.deepStrictEqual(obj, [
-        {
-          path1: {
-            path2: {
-              path3: '로그인'
-            }
-          }
-        },
-        {
-          path1: {
-            path2: {
-              path3: 'login'
-            }
+      {
+        path1: {
+          path2: {
+            path3: '로그인'
           }
         }
-      ])
+      },
+      {
+        path1: {
+          path2: {
+            path3: 'login'
+          }
+        }
+      }
+    ])
   })
   it('a array of text and path to Object (with language number option)', function () {
     const obj = textPathsAryToObj([[ '로그인', 'login', 'path1', 'path2', 'path3' ]], { numberOfLanguageColumn: 2 })
     assert.deepStrictEqual(obj, [
-        {
-          path1: {
-            path2: {
-              path3: '로그인'
-            }
-          }
-        },
-        {
-          path1: {
-            path2: {
-              path3: 'login'
-            }
+      {
+        path1: {
+          path2: {
+            path3: '로그인'
           }
         }
-      ])
+      },
+      {
+        path1: {
+          path2: {
+            path3: 'login'
+          }
+        }
+      }
+    ])
   })
   it('a array of text and path to Object (has one language column)', function () {
     const obj = textPathsAryToObj([[ '로그인', 'path1', 'path2', 'path3' ]], { numberOfLanguageColumn: 1 })
     assert.deepStrictEqual(obj, [
-        {
-          path1: {
-            path2: {
-              path3: '로그인'
-            }
+      {
+        path1: {
+          path2: {
+            path3: '로그인'
           }
         }
-      ])
+      }
+    ])
   })
 })
-
